56cfeef9d9d3987219abb2b72ad21f2afd5934c2
Andrianos Papamarkou <papamarkoua@gmail.com>
2024-09-10 23:19:02 +0300

    fixing chdir issue

--- src/appimage-integrator-extract.sh
+++ src/appimage-integrator-extract.sh
@@ -48,7 +48,8 @@ APPIMAGE_NAME="${APPIMAGE_FULL_NAME%%.*}"
 notify-send "Appimage integrator" "Setting up $APPIMAGE_NAME"
 
 # Create a unique temporary working directory in ~/tmp
-WORKDIR="$HOME/tmp/$APPIMAGE_NAME/"
+# WORKDIR="$HOME/tmp/$APPIMAGE_NAME/"
+WORKDIR="$APPIMAGE_NAME"
 mkdir -p "$WORKDIR"
 
 # Change to the working directory
